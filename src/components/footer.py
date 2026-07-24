c0b191778130b88debe7257d6f584f0ac1ddac99
1Maazansari <maazansari260@gmail.com>
2026-07-24 16:44:13 +0530

    Initial commit: AttendFusion AI

---
+++ src/components/footer.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+
+def footer_home():
+    st.markdown("""
+        <div style="margin-top:2rem; text-align:center;">
+            <p style="font-weight:bold; color:white;">
+                © 2026 AttendFusion AI | Developed by Maaz Ansari
+            </p>
+        </div>
+    """, unsafe_allow_html=True)
+
+
+def footer_dashboard():
+    st.markdown("""
+        <div style="margin-top:2rem; text-align:center;">
+            <p style="font-weight:bold; color:black;">
+                © 2026 AttendFusion AI | Developed by Maaz Ansari
+            </p>
+        </div>
+    """, unsafe_allow_html=True)
